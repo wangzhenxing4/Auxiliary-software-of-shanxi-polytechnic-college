ed630ae8ba5fc7a87fc897aa0461fdd2eff17555
王振兴 <wangzhenxing4@gmail.com>
2024-08-09 15:36:56 +0800

    Delete config.py
    Signed-off-by: 王振兴 <wangzhenxing4@gmail.com>

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-student_id = os.environ.get("STUDENT_ID")
-password = os.environ.get("PASSWORD")
-check_in_address_school = os.environ.get("SCHOOL_ADDRESS")
-check_in_address_home = os.environ.get("HOLIDAY_ADDRESS")
-token = os.environ.get("PUSH_MESSAGE_TOKEN")
-CHECK_IN_TIME = '放假'
