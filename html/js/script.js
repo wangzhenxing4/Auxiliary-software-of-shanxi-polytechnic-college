993fb0091c31621e113a7bb2d453d4c68522af1c
wangzhenxingdemacbookair <wangzhenxing4@gmail.com>
2024-09-05 21:18:08 +0800

    添加网页

---
+++ html/js/script.js
@@ -0,0 +1,5 @@
+// JavaScript functionality can be added here
+// For now, let's add a simple alert when the page loads
+document.addEventListener('DOMContentLoaded', () => {
+    alert('Welcome to Shanxi Zhiye Jishu Xueyuan!');
+});
